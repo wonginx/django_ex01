40a52036085f0add98cd0f8687334cc4e6ffe7ed
jinwonn <nngg375@naver.com>
2021-07-23 11:35:56 +0900

    ver 1-5

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,18 +1,25 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-# Create your views here.
-from django.urls import reverse, reverse_lazy
-from accountapp.models import NewModel
-
-from django.views.generic import CreateView
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+
+from accountapp.decorators import account_ownership_required
+from accountapp.forms import AccountCreationForm
+from accountapp.models import NewModel
+from django.utils.decorators import method_decorator
 
 
+
+#  login_required 의 주소 기본값은 accounts
+@login_required
 def hello_world(request):
+    # 로그인 여부 확인
     if request.method == 'POST':
         temp = request.POST.get('input_text')
+
         new_model = NewModel()
         new_model.text = temp
         new_model.save()
@@ -22,9 +29,37 @@ def hello_world(request):
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list': data_list})
 
+    # 로그인이 안되어 있다면 로그인페이지로 이동
+
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+
+has_ownership = [login_required, account_ownership_required]
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/delete.html'
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountCreationForm
+    context_object_name = 'target_user'
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = 'accountapp/update.html'
--- ex01/settings.py
+++ ex01/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accountapp',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -139,6 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
