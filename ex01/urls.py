e19b571bba5a3f14889f9fa815bc8da225fdb517
jinwonn <nngg375@naver.com>
2021-07-27 12:59:09 +0900

    ver 1-8 image

--- ex01/urls.py
+++ ex01/urls.py
@@ -1,4 +1,4 @@
-"""ex01 URL Configuration
+"""Project_1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include\
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
-
-
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 이미지 경로만들기
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accounapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form): # 검증이 완료되면 할당 서버의 유저
+        form.instance.user = self.request.user
+        return super().form_valid(form)
