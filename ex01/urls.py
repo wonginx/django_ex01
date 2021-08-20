162311c51f6a5b26fc15bf6926fd2e44e3675eb5
jinwonn <nngg375@naver.com>
2021-08-20 17:12:28 +0900

    ver 4-1 / subscriapp 생성 / 구독기능 설정 / 구독기능연결

--- ex01/urls.py
+++ ex01/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
+    path('subscribes/', include('subscribeapp.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 이미지 경로만들기
